6429b095a00c113347af9696c997b88f82f30af7
lipstickof87 <2380588373@qq.com>
2025-01-25 20:09:24 +0800

    client初始化配置

---
+++ client/src/pages/chat/index.tsx
@@ -0,0 +1,8 @@
+const Chat = () => {
+  return (
+    <>
+      <h1>Chat</h1>
+    </>
+  );
+};
+export default Chat;
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,7 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vite.dev/config/
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    open: true,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+  },
+});

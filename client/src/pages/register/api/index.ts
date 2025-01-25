e028a64d70c04503737a5f711141c6df132cc4e2
lipstickof87 <2380588373@qq.com>
2025-01-25 22:26:48 +0800

    前端登录封装api和storage

---
+++ client/src/pages/register/api/index.ts
@@ -0,0 +1,6 @@
+import Request from '@/utils/request';
+import { RegisterParams, RegisterResponseData } from './type';
+export const handleRegister = async (data: RegisterParams) => {
+  const res = await Request.post<RegisterParams, RegisterResponseData>('/auth/register', data);
+  return res.data;
+}

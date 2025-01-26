8a1021a03594c13b467c0b4138cbba9146537650
lipstickof87 <2380588373@qq.com>
2025-01-26 11:25:24 +0800

    前端ui增加icon

--- client/src/pages/container/index.tsx
+++ client/src/pages/container/index.tsx
@@ -1,7 +1,12 @@
 import styles from './index.module.less';
+import { Tooltip } from 'antd';
+import { useState } from 'react';
+import { iconList } from './variable';
 import { userStorage, clearSessionStorage } from '@/common/storage';
+
 const Container = () => {
   const { username, avatar, phone, signature } = JSON.parse(userStorage.getItem() || '{}');
+  const [currentIcon, setCurrentIcon] = useState<string>('icon-message');
   return (
     <>
       <div className={styles.container}>
@@ -9,8 +14,38 @@ const Container = () => {
           <div className={styles.avatar}>
             <img src={avatar} alt="" />
           </div>
-          <div className={styles.topicons}></div>
-          <div className={styles.bottomicons}></div>
+        <div className={styles.iconList}>
+            <ul className={styles.topIcons}>
+              {iconList.slice(0, 5).map((item) => {
+                return (
+                  <Tooltip key={item.text} placement="bottomLeft" title={item.text} arrow={false}>
+                    <li
+                      className={`iconfont ${item.icon}`}
+                      onClick={() => {
+                        setCurrentIcon(item.icon);
+                      }}
+                      style={{ color: currentIcon === item.icon ? '#07c160' : '#979797' }}
+                    ></li>
+                  </Tooltip>
+                );
+              })}
+            </ul>
+            <ul className={styles.bottomIcons}>
+              {iconList.slice(5, 8).map((item) => {
+                return (
+                  <Tooltip key={item.text} placement="bottomLeft" title={item.text} arrow={false}>
+                    <li
+                      className={`iconfont ${item.icon}`}
+                      onClick={() => {
+                        setCurrentIcon(item.text);
+                      }}
+                      style={{ color: currentIcon === item.text ? '#07c160' : '#979797' }}
+                    ></li>
+                  </Tooltip>
+                );
+              })}
+            </ul>
+          </div>
         </div>
         <div className={styles.middleContainer}></div>
         <div className={styles.rightContainer}></div>
---
+++ client/src/pages/container/variable.ts
@@ -0,0 +1,34 @@
+export const iconList = [
+  {
+    icon: 'icon-liaotian',
+    text: '聊天',
+  },
+  {
+    icon: 'icon-tongxunlu',
+    text: '通讯录',
+  },
+  {
+    icon: 'icon-mofang',
+    text: '收藏',
+  },
+  {
+    icon: 'icon-wenjianjia',
+    text: '聊天文件',
+  },
+  {
+    icon: 'icon-pengyouquan',
+    text: '朋友圈',
+  },
+  {
+    icon: 'icon-xiaochengxu',
+    text: '小程序面板',
+  },
+  {
+    icon: 'icon-shouji',
+    text: '手机',
+  },
+  {
+    icon: 'icon-guanji',
+    text: '更多',
+  },
+];

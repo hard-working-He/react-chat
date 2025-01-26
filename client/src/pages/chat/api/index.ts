24b85d4ed02c8083d4946a5403c1fa8263bce477
lipstickof87 <2380588373@qq.com>
2025-01-26 09:09:20 +0800

    redirect

---
+++ client/src/pages/chat/api/index.ts
@@ -0,0 +1,6 @@
+import Request from '@/utils/request';
+import { IUserInfo } from './type';
+export const handleLogout = async (data: IUserInfo) => {
+  const res = await Request.post<IUserInfo>('/auth/logout', data);
+  return res.data;
+}
--- client/src/pages/login/api/index.ts
+++ client/src/pages/login/api/index.ts
@@ -1,6 +1,6 @@
 import Request from '@/utils/request';
-import { LoginParams, LoginResponseData } from './type';
-export const handleLogin = async (data: LoginParams) => {
-  const res = await Request.post<LoginParams, LoginResponseData>('/auth/login', data);
+import { ILoginParams, ILoginResponseData } from './type';
+export const handleLogin = async (data: ILoginParams) => {
+  const res = await Request.post<ILoginParams, ILoginResponseData>('/auth/login', data);
   return res.data;
 }
